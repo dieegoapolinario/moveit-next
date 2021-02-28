6fd2b1a143ef2c8b8e0a58d61c094ae7da216599
Diego Apolinário <dieego.apolinario@gmail.com>
2021-02-27 22:35:18 -0300

    update path

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile(){
+  const { level } = useContext(ChallengesContext);
+
+  return(
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/dieegoapolinario.png" alt="Diego Apolinário"/>
+      <div>
+        <strong>Diego Apolinário</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
